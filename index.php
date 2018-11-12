ca498410b0b1e2dcd0bb2f6113c028dd2db91efc
TrungNV <trungnv@egdgroup.com>
2018-11-12 15:06:07 +0700

    add file index.go

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trungnv
+ * Date: 11/12/2018
+ * Time: 3:05 PM
+ */
+class PHP_Check{
+    public function __construct()
+    {
+
+    }
+
+    public function index(){
+
+    }
+}
