c8bcef18e13d9955985096886cc6be3c80bdbd05
TrungNV <trungnv@egdgroup.com>
2018-11-12 15:07:08 +0700

    add index controller

---
+++ template/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trungnv
+ * Date: 11/12/2018
+ * Time: 3:06 PM
+ */
+class IndexController{
+
+    public function __construct()
+    {
+    }
+
+    public function index(){
+
+    }
+}
